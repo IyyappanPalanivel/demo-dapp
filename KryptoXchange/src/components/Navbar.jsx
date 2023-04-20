219697f6a31a3bb59af8afbf6b1a2382059c8ecf
IyyappanPalanivel <iyyappan.p@technotackle.com>
2023-04-20 13:38:52 +0530

    - Connect wallet function done

--- KryptoXchange/src/components/Navbar.jsx
+++ KryptoXchange/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import vite from '../../public/vite.svg'
 
+const NavBarItem = ({ title, classprops }) => (
+  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+);
 
 function Navbar() {
   return (
@@ -9,11 +12,13 @@ function Navbar() {
       <div className='md:flex-1 flex-initial'>
         <img src={vite} alt='logo' className='cursor-pointer' />
       </div>
-      <ul className='text-white md:flex'>
-        <li>Market</li>
-        <li>Exchange</li>
-        <li>Tutorials</li>
-        <li>Wallets</li>
+      <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+        ))}
+        <li className='bg-[#2952e3] px-7 py-2 rounded-full hover:bg-[#2546bd] cursor-pointer'>
+          Login
+        </li>
       </ul>
     </nav>
   )
--- KryptoXchange/src/components/Welcome.jsx
+++ KryptoXchange/src/components/Welcome.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { AiFillPlayCircle } from "react-icons/ai";
 
 function Welcome() {
+
+  const connectWallet = () => {
+    if (window.ethereum) {
+      // Do something 
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(res => {
+          // Return the address of the wallet
+          console.log(res[0])
+        })
+    } else {
+      alert("Install Metamask Wallet Extension!!")
+    }
+  }
+
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className='flex w-full justify-center items-center'>
+      <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+        {/* Send Crypto Section */}
+        <div className='flex flex-1 justify-start items-start flex-col mf:mr-10'>
+          <h1 className='text-3xl  text-white text-gradient'>
+            Send Crypto <br /> across the World
+          </h1>
+          <p className='text-white text-left mt-6 font-light md:w-9/12 w-11/12'>
+            Explore the crypto world. Buy and Sell cryptocurrencies easily on Krypto
+          </p>
+        </div>
+
+        {/* Connect Wallet */}
+        <button
+          type='button'
+          onClick={connectWallet}
+          className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+          <AiFillPlayCircle className="text-white mr-2" />
+          <p className="text-white text-base font-semibold">
+            Connect Wallet
+          </p>
+        </button>
+
+        {/* Grid */}
+        <div class="grid grid-cols-3">
+          <div className='border border-white text-white items-center justify-center p-4 rounded-tl-2xl'>Reliability</div>
+          <div className='border border-white text-white items-center justify-center p-4'> Security</div>
+          <div className='border border-white text-white items-center justify-center p-4 rounded-tr-2xl'>Ethereum</div>
+          <div className='border border-white text-white items-center justify-center p-4 rounded-bl-2xl'>Web 3.0</div>
+          <div className='border border-white text-white items-center justify-center p-4'>Low Fees</div>
+          <div className='border border-white text-white items-center justify-center p-4 rounded-br-2xl'>Blockchain</div>
+        </div>
+      </div>
     </div>
   )
 }
